33dcb463740e05bdc486ffd2410c5563257349cb
Btforce <tashirobradley@gmail.com>
2019-09-20 13:07:57 -0700

    Made Map Made KeyboardListener Class

--- app/src/main/java/com/example/soundboard/SoundBoardActivity.java
+++ app/src/main/java/com/example/soundboard/SoundBoardActivity.java
@@ -8,9 +8,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class SoundBoardActivity extends AppCompatActivity {
+public class SoundBoardActivity extends AppCompatActivity implements View.OnClickListener{
+
     private Button a;
     private Button b;
     private Button b_flat;
@@ -20,6 +26,7 @@ public class SoundBoardActivity extends AppCompatActivity {
     private int aNote;
     private int bNote;
     private int bFlatNote;
+    private Map<Integer, Integer> noteMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +51,92 @@ public class SoundBoardActivity extends AppCompatActivity {
         aNote = soundPool.load(this, R.raw.scalea, 1);
         bNote = soundPool.load(this, R.raw.scaleb, 1);
         bFlatNote = soundPool.load(this, R.raw.scalebb, 1);
+
+        noteMap = new HashMap<>();
+        noteMap.put(a.getId(), aNote);
+        noteMap.put(b_flat.getId(), bFlatNote);
+        noteMap.put(b.getId(), bNote);
     }
 
     private void setListeners() {
-        a.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//        a.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+//                float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+//                float maxVolume = (float) audioManager
+//                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+//                float volume = actualVolume / maxVolume;
+//                // Is the sound loaded already?
+//                if (loaded) {
+//                    soundPool.play(aNote, volume, volume, 1, 0, 1f);
+//                    Log.e("Test", "Played sound");
+//                }
+//            }
+//        });
+//
+//        b_flat.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+//                float actualVolume = (float) audioManager
+//                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+//                float maxVolume = (float) audioManager
+//                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+//                float volume = actualVolume / maxVolume;
+//                // Is the sound loaded already?
+//                if (loaded) {
+//                    soundPool.play(bFlatNote, volume, volume, 1, 0, 1f);
+//                    Log.e("Test", "Played sound");
+//                }
+//            }
+//        });
+//
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+//                float actualVolume = (float) audioManager
+//                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+//                float maxVolume = (float) audioManager
+//                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+//                float volume = actualVolume / maxVolume;
+//                // Is the sound loaded already?
+//                if (loaded) {
+//                    soundPool.play(bNote, volume, volume, 1, 0, 1f);
+//                    Log.e("Test", "Played sound");
+//                }
+//            }
+//
+//        });
+        KeyboardListener keyboardListener = new KeyboardListener();
+        a.setOnClickListener(keyboardListener);
+        b.setOnClickListener(keyboardListener);
+        b_flat.setOnClickListener(keyboardListener);
+
+        a.setOnClickListener(this);
+        b.setOnClickListener(this);
+        b_flat.setOnClickListener(this);
+    }
+
+    private void delay(int millisDelay){
+        try {
+            Thread.sleep(millisDelay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void wireWidgets() {
+        a = findViewById(R.id.button_main_A);
+        b_flat = findViewById(R.id.button_main_Bflat);
+        b = findViewById(R.id.button_main_B);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button_main_A : {
                 AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
                 float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
                 float maxVolume = (float) audioManager
@@ -60,12 +147,10 @@ public class SoundBoardActivity extends AppCompatActivity {
                     soundPool.play(aNote, volume, volume, 1, 0, 1f);
                     Log.e("Test", "Played sound");
                 }
+                break;
             }
-        });
 
-        b_flat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            case R.id.button_main_Bflat : {
                 AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
                 float actualVolume = (float) audioManager
                         .getStreamVolume(AudioManager.STREAM_MUSIC);
@@ -77,12 +162,10 @@ public class SoundBoardActivity extends AppCompatActivity {
                     soundPool.play(bFlatNote, volume, volume, 1, 0, 1f);
                     Log.e("Test", "Played sound");
                 }
+                break;
             }
-        });
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            case R.id.button_main_B : {
                 AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
                 float actualVolume = (float) audioManager
                         .getStreamVolume(AudioManager.STREAM_MUSIC);
@@ -94,13 +177,20 @@ public class SoundBoardActivity extends AppCompatActivity {
                     soundPool.play(bNote, volume, volume, 1, 0, 1f);
                     Log.e("Test", "Played sound");
                 }
+                break;
             }
-        });
+        }
     }
 
-    private void wireWidgets() {
-        a = findViewById(R.id.button_main_A);
-        b_flat = findViewById(R.id.button_main_Bflat);
-        b = findViewById(R.id.button_main_B);
+    private class KeyboardListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            // read from my map -- looking up which button was pressed and play the associated note
+            int songId = noteMap.get(view.getId());
+            if(songId != 0){
+                soundPool.play(songId, 1, 1, 1, 0, 1);
+            }
+        }
     }
 }
